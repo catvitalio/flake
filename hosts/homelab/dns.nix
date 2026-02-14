732d6501f5c234968cbdb1a1ca7655d5db4a1e1a
Vitaly Kotov <catvitalio@gmail.com>
2026-02-14 19:20:59 +0700

    add adguard home

--- hosts/homelab/dns.nix
+++ hosts/homelab/dns.nix
@@ -1,4 +1,10 @@
+{ lib, config, ... }:
+
 let
+  domain = "adguard.catvitalio.com";
+  address = "127.0.0.1";
+  port = 5354;
+  dnsPort = 5353;
   constants = import ./constants.nix;
 in
 {
@@ -10,10 +16,50 @@ in
       bind-interfaces = true;
       port = 53;
       server = [
-        "77.88.8.8"
-        "77.88.8.1"
+        "${address}#${toString dnsPort}"
       ];
       cache-size = 10000;
     };
   };
+
+  services.adguardhome = {
+    enable = true;
+    host = address;
+    port = port;
+    settings = {
+      dns = {
+        bind_hosts = [ address ];
+        port = dnsPort;
+        upstream_dns = [
+          "https://dns.google/dns-query"
+          "https://cloudflare-dns.com/dns-query"
+        ];
+      };
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+        parental_enabled = false;
+        safe_search.enabled = false;
+      };
+    };
+  };
+
+  services.nginx.virtualHosts.${domain} = {
+    useACMEHost = domain;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://${address}:${toString port}";
+    };
+  };
+
+  security.acme.certs.${domain} = {
+    dnsProvider = "cloudflare";
+    environmentFile = config.age.secrets.acmeEnv.path;
+    reloadServices = [ "nginx" ];
+  };
+
+  services.dnsmasq.settings.address = lib.mkAfter [
+    "/${domain}/${constants.wireguard.address}"
+  ];
+
 }
