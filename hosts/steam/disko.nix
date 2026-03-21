f07a14bdcaaf490b204cfce6ddaf75a6eb260ddd
Vitaly Kotov <catvitalio@gmail.com>
2026-03-21 17:00:37 +0700

    fix disko

--- hosts/steam/disko.nix
+++ hosts/steam/disko.nix
@@ -7,9 +7,8 @@
       content = {
         type = "gpt";
         partitions = {
-          esp = {
-            priority = 1;
-            size = "1G";
+          ESP = {
+            size = "512M";
             type = "EF00";
             content = {
               type = "filesystem";
@@ -19,17 +18,8 @@
             };
           };
 
-          swap = {
-            priority = 3;
-            size = "34G";
-            content = {
-              type = "swap";
-            };
-          };
-
           root = {
-            priority = 2;
-            size = "1.8T";
+            size = "100%";
             content = {
               type = "filesystem";
               format = "ext4";
