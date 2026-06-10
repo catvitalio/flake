024bacc46a5e70ca7346a4bbdacbe103142faefa
Vitaly Kotov <catvitalio@gmail.com>
2026-06-10 13:03:40 +0700

    forza proton

--- hosts/steam/proton.nix
+++ hosts/steam/proton.nix
@@ -33,6 +33,8 @@
         SteamDeck = "0";
         RADV_EXPERIMENTAL = "heap";
         MESA_SHADER_CACHE_MAX_SIZE = "10G";
+        DISABLE_LOW_LATENCY_LAYER = "1";
+        VKD3D_CONFIG = "enable_experimental_features,descriptor_heap,avoid_image_buffer_aliasing,null_buffer_siblings";
         radv_wait_for_vm_map_updates = "true";
       };
     };
