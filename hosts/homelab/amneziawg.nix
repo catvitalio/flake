172961cfd076dea7f111f13f3ce65a8e8a4e5846
Vitaly Kotov <catvitalio@gmail.com>
2026-06-26 00:15:43 +0700

    homelab: AWG server replaces wg0 Keenetic client

--- hosts/homelab/amneziawg.nix
+++ hosts/homelab/amneziawg.nix
@@ -8,9 +8,12 @@ let
   interface = "awg0";
   sock = "/run/amneziawg/${interface}.sock";
   port = 13231;
+  serverIp = "10.100.0.1";
+  subnet = "10.100.0.0/24";
   mobilePublicKeyHex = "b4cc1bd43fb3444b8cb703c85d787737c1b3ff53aaa1b6c4f374048031993d66";
   mobileIp = "10.100.0.10";
   ip = "${pkgs.iproute2}/bin/ip";
+  ipt = "${pkgs.iptables}/bin/iptables";
   socat = "${pkgs.socat}/bin/socat";
 
   configureScript = pkgs.writeShellScript "awg-configure" ''
@@ -27,8 +30,18 @@ let
     printf 'set=1\nprivate_key=%s\nlisten_port=${toString port}\njc=4\njmin=40\njmax=70\ns1=0\ns2=0\nh1=1\nh2=2\nh3=3\nh4=4\nreplace_peers=true\npublic_key=${mobilePublicKeyHex}\nallowed_ip=${mobileIp}/32\n\n' \
       "$PRIV_HEX" | ${socat} - UNIX-CONNECT:${sock}
 
+    ${ip} addr add ${serverIp}/24 dev ${interface}
     ${ip} route replace ${mobileIp}/32 dev ${interface}
     ${ip} link set ${interface} up
+
+    ${ipt} -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+    ${ipt} -t nat -A POSTROUTING -s ${subnet} ! -o ${interface} -j MASQUERADE
+  '';
+
+  cleanupScript = pkgs.writeShellScript "awg-cleanup" ''
+    ${ipt} -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu 2>/dev/null || true
+    ${ipt} -t nat -D POSTROUTING -s ${subnet} ! -o ${interface} -j MASQUERADE 2>/dev/null || true
+    ${ip} link delete ${interface} 2>/dev/null || true
   '';
 in
 {
@@ -46,10 +59,15 @@ in
       ExecStartPre = "-${ip} link delete ${interface}";
       ExecStart = "${pkgs.amneziawg-go}/bin/amneziawg-go ${interface}";
       ExecStartPost = "${configureScript}";
-      ExecStopPost = "-${ip} link delete ${interface}";
+      ExecStopPost = "${cleanupScript}";
     };
   };
 
   networking.firewall.allowedUDPPorts = [ port ];
   networking.firewall.trustedInterfaces = [ interface ];
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 }
--- hosts/homelab/wireguard.nix
+++ hosts/homelab/wireguard.nix
@@ -6,56 +6,18 @@
 }:
 
 let
-  homeInterface = "wg0";
   workInterface = "wg1";
   work = import "${secrets}/work.nix";
   iptables = "${pkgs.iptables}/bin/iptables";
-  ip6tables = "${pkgs.iptables}/bin/ip6tables";
 in
 {
-  networking.wireguard.interfaces = {
-    ${homeInterface} = {
-      ips = [
-        "10.100.0.2/24"
-        "fd00:100::2/64"
-      ];
-      privateKeyFile = config.age.secrets.wireguardKey.path;
-      peers = [
-        {
-          publicKey = "mL2pYNjMdCjaW1CCFTVxeKUIbjlv3/Bg5vw0yfEO6H8=";
-          allowedIPs = [
-            "10.100.0.0/24"
-            "fd00:100::/64"
-          ];
-          endpoint = "192.168.1.1:51820";
-          persistentKeepalive = 25;
-        }
-      ];
-      postSetup = ''
-        ${iptables} -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-        ${ip6tables} -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-      '';
-      postShutdown = ''
-        ${iptables} -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu 2>/dev/null || true
-        ${ip6tables} -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu 2>/dev/null || true
-      '';
-    };
-    ${workInterface} = {
-      ips = [ work.address ];
-      privateKeyFile = config.age.secrets.wireguardWorkKey.path;
-      peers = work.peers;
-      postSetup = "${iptables} -t nat -A POSTROUTING -o ${workInterface} -j MASQUERADE";
-      postShutdown = "${iptables} -t nat -D POSTROUTING -o ${workInterface} -j MASQUERADE 2>/dev/null || true";
-    };
+  networking.wireguard.interfaces.${workInterface} = {
+    ips = [ work.address ];
+    privateKeyFile = config.age.secrets.wireguardWorkKey.path;
+    peers = work.peers;
+    postSetup = "${iptables} -t nat -A POSTROUTING -o ${workInterface} -j MASQUERADE";
+    postShutdown = "${iptables} -t nat -D POSTROUTING -o ${workInterface} -j MASQUERADE 2>/dev/null || true";
   };
 
-  networking.firewall.trustedInterfaces = [
-    homeInterface
-    workInterface
-  ];
-
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
+  networking.firewall.trustedInterfaces = [ workInterface ];
 }
