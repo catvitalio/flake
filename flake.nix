c3860cd3adc267705adfb64f42f2f5b4bdd5b0c9
Vitaly Kotov <catvitalio@gmail.com>
2026-07-31 23:28:16 +0700

    revert steam

--- flake.nix
+++ flake.nix
@@ -3,8 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
     disko.url = "github:nix-community/disko";
+
+    jovian = {
+      url = "github:Jovian-Experiments/Jovian-NixOS";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    nix-gaming-edge = {
+      url = "github:powerofthe69/nix-gaming-edge";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     secrets = {
       url = "git+ssh://git@github.com/catvitalio/secrets.git";
@@ -16,8 +27,11 @@
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       agenix,
       disko,
+      jovian,
+      nix-gaming-edge,
       secrets,
       ...
     }:
@@ -33,15 +47,29 @@
       };
     in
     {
+      nixosModules = {
+        wrappedProton = import ./modules/wrapped-proton.nix;
+      };
+
       nixosConfigurations = {
         homelab = mkHost nixpkgs {
           modules = [
             disko.nixosModules.disko
-            ./lib/reverse-proxy.nix
+            ./modules/reverse-proxy.nix
             ./hosts/homelab
           ];
         };
 
+        steam = mkHost nixpkgs-unstable {
+          specialArgs = {
+            inherit nix-gaming-edge;
+          };
+          modules = [
+            disko.nixosModules.disko
+            jovian.nixosModules.default
+            ./hosts/steam
+          ];
+        };
       };
     };
 }
