e0456431c1f554ba80344c63daa1109551747bb0
Vitaly Kotov <catvitalio@gmail.com>
2026-08-14 01:23:05 +0700

    awg remove

--- hosts/homelab/wireguard.nix
+++ hosts/homelab/wireguard.nix
@@ -8,45 +8,14 @@
 let
   work = import "${secrets}/work.nix";
   iptables = "${pkgs.iptables}/bin/iptables";
+  port = 51820;
 in
 {
-  boot.extraModulePackages = with config.boot.kernelPackages; [ amneziawg ];
-  environment.systemPackages = with pkgs; [ amneziawg-tools ];
-
-  networking.wireguard.interfaces.wg1 = {
-    ips = [ work.address ];
-    privateKeyFile = config.age.secrets.wireguardWorkKey.path;
-    peers = work.peers;
-    mtu = 1280;
-    postSetup = "${iptables} -t nat -A POSTROUTING -o wg1 -j MASQUERADE";
-    postShutdown = "${iptables} -t nat -D POSTROUTING -o wg1 -j MASQUERADE 2>/dev/null || true";
-  };
-
-  networking.wg-quick.interfaces.awg0 = {
-    type = "amneziawg";
-    address = [ "10.100.0.1/24" ];
-    listenPort = 47891;
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.100.0.1/32" ];
+    listenPort = port;
     privateKeyFile = config.age.secrets.wireguardKey.path;
     mtu = 1280;
-
-    postUp = ''
-      ${iptables} -t nat -A POSTROUTING -s 10.100.0.0/24 ! -o awg0 -j MASQUERADE
-    '';
-    preDown = ''
-      ${iptables} -t nat -D POSTROUTING -s 10.100.0.0/24 ! -o awg0 -j MASQUERADE 2>/dev/null || true
-    '';
-
-    extraOptions = {
-      Jc = 4;
-      Jmin = 40;
-      Jmax = 70;
-      S1 = 0;
-      S2 = 0;
-      H1 = 1;
-      H2 = 2;
-      H3 = 3;
-      H4 = 4;
-    };
 
     peers = [
       {
@@ -61,16 +30,6 @@ in
         publicKey = "sTvH3WJ8OHwBynHwdBmhuiaLT7bOniFFu0MowpOk2Vk=";
         allowedIPs = [ "10.100.0.12/32" ];
       }
-    ];
-  };
-
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.100.0.1/32" ];
-    listenPort = 51820;
-    privateKeyFile = config.age.secrets.wireguardKey.path;
-    mtu = 1280;
-
-    peers = [
       {
         publicKey = "aevcJc31KAERcLYbJJVIAosRppFTyKsBv0aH71wAIS8=";
         allowedIPs = [ "10.100.0.13/32" ];
@@ -82,14 +41,19 @@ in
     ];
   };
 
-  networking.firewall.allowedUDPPorts = [
-    47891
-    51820
-  ];
+  networking.wireguard.interfaces.wg1 = {
+    ips = [ work.address ];
+    privateKeyFile = config.age.secrets.wireguardWorkKey.path;
+    peers = work.peers;
+    mtu = 1280;
+    postSetup = "${iptables} -t nat -A POSTROUTING -o wg1 -j MASQUERADE";
+    postShutdown = "${iptables} -t nat -D POSTROUTING -o wg1 -j MASQUERADE 2>/dev/null || true";
+  };
+
+  networking.firewall.allowedUDPPorts = [ port ];
   networking.firewall.trustedInterfaces = [
-    "wg1"
-    "awg0"
     "wg0"
+    "wg1"
   ];
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 }
